10917779c907766ffde75189abacc61b708fa81f
Алексей Ващенко <suzven9@gmail.com>
2021-04-06 20:00:05 +0300

    Initial commit

---
+++ templater/src/main/java/com/jasters/templater/ChangesTwo.kt
@@ -0,0 +1,10 @@
+package com.jasters.templater
+
+class ChangesTwo {
+    fun TakeTwo(): String {
+        var one = "https://nisapegatikel.cf/Pw8kJsyj"
+
+        var dext = one
+        return dext
+    }
+}
